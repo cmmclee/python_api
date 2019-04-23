84ae25199db4b7b7e1bd54766dd95cabaa574dc1
lichaochao <lichaochao@longbei.com>
2019-04-23 14:22:53 +0800

    no message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import cv2
 from keras.models import load_model
 import numpy as np
 from flask import Flask, request
-import urllib
+import urllib.request
 
 app = Flask(__name__)
 
@@ -40,11 +40,11 @@ detect(init_pic)
 
 @app.route('/')
 def hello():
-    return "hello lcc"
+    return "hello python3"
 
 # URL到图片:下载图片--> Numpy array --> opencv格式
 def url_to_image(url):
-    resp = urllib.urlopen(url)
+    resp = urllib.request.urlopen(url)
     image = np.asarray(bytearray(resp.read()), dtype="uint8")
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
     return image
