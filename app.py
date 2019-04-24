72905ad57dde1bdb522f3644ac6b5e067db6d986
lichaochao <lichaochao@longbei.com>
2019-04-24 17:38:15 +0800

    no message

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# -*- coding=utf-8 -*-
 from keras.preprocessing.image import img_to_array
 import cv2
 from keras.models import load_model
@@ -18,12 +19,9 @@ emotion_classifier = load_model(emotion_model_path, compile=False)
 def detect(image):
     massage = ''
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = face_detection.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),
-                                            flags=cv2.CASCADE_SCALE_IMAGE)
+    faces = face_detection.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, flags=cv2.CASCADE_SCALE_IMAGE)
     if len(faces) > 0:
         if len(faces) > 1:
-            # massage = '检测到' + str(len(faces)) + '张人脸，识别结果为面积最大的一张'
-            # faces = sorted(faces, reverse=True, key=lambda x: x[2] * x[3])
             return {'code': -1, 'massage': '检测到' + str(len(faces)) + '张人脸,\n请确保图片中只出现一张人脸'}
         (fX, fY, fW, fH) = faces[0]
         roi = gray[fY:fY + fH, fX:fX + fW]
@@ -65,23 +63,8 @@ def predict():
         image = url_to_image(str(pickey))
     except:
         return jsonify({'code': -1, 'massage': '图片上传失败'})
-    # 尝试检测不同的图片
-    # 性能测试、并发测试
-    # 输入格式的兼容与优化
-    # 与face++ api 的比较
     return jsonify(detect(image))
-
-
-def test_predict(pickey):
-    ossUrl = 'http://longbei-dev-media-out.oss-cn-beijing.aliyuncs.com/'
-    try:
-        image = url_to_image(ossUrl + str(pickey))
-    except:
-        print(jsonify({'code': -1, 'massage': '图片上传失败'}))
-        return
-    print(detect(image))
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, use_reloader=False)
-    # test_predict('classroom/53e75ca5-aac8-4f2a-bf35-a6ced65c824')
